14f776684523af9d44905e5c4af12b2055914880
chenshtynmetz <jiayhb@gmail.com>
2022-03-24 18:58:44 +0200

    commit monitor

--- monitor.py
+++ monitor.py
@@ -1,5 +1,6 @@
 import platform
 import os
+import subprocess
 import time
 from datetime import datetime
 
@@ -36,12 +37,21 @@ class Monitor:
                 curr_time = "\n$" + str(datetime.now()) + "\n"
                 prev_proc = curr_proc
                 if self.system == "Windows":
-                    curr_proc = os.popen("tasklist").read()
+                    try:
+                        curr_proc = subprocess.check_output("net start", shell=True, encoding="unicode_escape")
+                    except AttributeError:
+                        pass
                 elif self.system == "Linux":
-                    curr_proc = os.popen("ps aux").read()
+                    curr_proc = os.popen("systemctl list-units --type=service").read()
                 self.compare(curr_proc, prev_proc, curr_time)
                 file.write(curr_time)
-                file.write(curr_proc)
+                curr_list = curr_proc.split("\n")
+                for i in range(len(curr_list) - 3):
+                    try:
+                        file.write(curr_list[i] + "\n")
+                        print("##\n" + curr_list[i])
+                    except UnicodeEncodeError:
+                        continue
                 file.write("\n~\n")
                 time.sleep(float(self.my_time))
 
@@ -51,12 +61,7 @@ class Monitor:
         curr_list = curr.split('\n')
         prev_id_list = [1, 1, 1, 1]
         curr_id_list = [1, 1, 1, 1]
-        if self.system == "Windows":
-            for i in range(4, len(prev_list)):  # get process by ID
-                prev_id_list.append(prev_list[i][26:33])
-            for i in range(4, len(curr_list)):
-                curr_id_list.append(curr_list[i][26:33])  # get process by ID
-        elif self.system == "Linux":
+        if self.system == "Linux":
             for i in range(4, len(prev_list)):  # get process by ID
                 prev_id_list.append(prev_list[i][8:16])
             for i in range(4, len(curr_list)):
@@ -65,16 +70,22 @@ class Monitor:
             file.write('\n' + curr_time + '\n')
             self.current_change = '\n' + curr_time + '\n'
             print('\n' + curr_time + '\n')
-            for i in range(4, len(prev_list)):
-                if prev_id_list[i] not in curr_id_list:
-                    file.write("stopped:" + '\t' + prev_list[i] + '\n')
-                    self.current_change = "stopped:" + '\t' + prev_list[i] + '\n'
-                    print("stopped:" + '\t' + prev_list[i] + '\n')
-            for i in range(4, len(curr_list)):
-                if curr_id_list[i] not in prev_id_list:
-                    file.write("started:" + '\t' + curr_list[i] + '\n')
-                    self.current_change = "started:" + '\t' + curr_list[i] + '\n'
-                    print("started:" + '\t' + curr_list[i] + '\n')
+            for i in range(len(prev_list) - 3):
+                if prev_list[i] not in curr_list:
+                    try:
+                        file.write("stopped:" + '\t' + prev_list[i] + '\n')
+                        self.current_change = "stopped:" + '\t' + prev_list[i] + '\n'
+                        print("stopped:" + '\t' + prev_list[i] + '\n')
+                    except UnicodeEncodeError:
+                        continue
+            for i in range(len(curr_list) - 3):
+                if curr_list[i] not in prev_list:
+                    try:
+                        file.write("started:" + '\t' + curr_list[i] + '\n')
+                        self.current_change = "started:" + '\t' + curr_list[i] + '\n'
+                        print("started:" + '\t' + curr_list[i] + '\n')
+                    except UnicodeEncodeError:
+                        continue
 
     def exit_monitor(self):
         self.flag = 0
